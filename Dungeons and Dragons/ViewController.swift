e104287f5b008414c2b3bc000121b03ec1dc7bfa
Jacob P Wang <59838504+jacobwang7@users.noreply.github.com>
2021-03-10 18:28:12 -0500

    D&D dice roller

--- Dungeons and Dragons/ViewController.swift
+++ Dungeons and Dragons/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceLabel.text = ""
     }
-
-
+    
+    @IBAction func fourButtonPressed(_ sender: UIButton) {
+        let diceRoll = Int.random(in: 1...sender.tag)
+        diceLabel.text = "You rolled a \(sender.tag)-sided dice and got a \(diceRoll)!"
+    }
+    
 }
 
